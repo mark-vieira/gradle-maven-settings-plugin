19f19994073a9ccbb0ee0512880165a02957bc7c
Baptiste Mesta <baptiste.mesta@gmail.com>
2022-02-15 10:54:00 +0100

    chore(release): add publication to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,10 @@ plugins {
     id("com.gradle.plugin-publish") version "0.14.0"
     `groovy-gradle-plugin`
     kotlin("jvm") version "1.4.32"
+    id("maven-publish")
 }
+
+group = "com.bonitasoft.gradle"
 
 repositories {
     mavenCentral()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.gradle.enterprise") version "3.1.1"
 }
 
+
 gradleEnterprise{
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
